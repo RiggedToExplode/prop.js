df58e9724136a7c061b29dcc87b36bd81850a9b2
RiggedToExplode <thomas.zent@gmail.com>
2019-06-16 22:13:30 -0700

    Add controls object in order to change movement keyCodes on the fly

--- annotated/modules/player.js
+++ annotated/modules/player.js
@@ -11,6 +11,12 @@ $P.Player = class extends $P.PhysicsProp {
     super(pos, mass);
 
     this._acceleration = 0.005;
+    this._controls = {
+      up: '38',
+      down: '40',
+      left: '37',
+      right: '39'
+    }
   }
 
 
@@ -18,22 +24,30 @@ $P.Player = class extends $P.PhysicsProp {
     this._acceleration = acc;
   }
 
+  set controls(controls) {
+    this._controls = controls;
+  }
+
   get acceleration() {
     return this._acceleration;
   }
 
+  get controls() {
+    return this._controls;
+  }
+
 
   beforeUpdate() {
-    if ($P.keys['38']) {
+    if ($P.keys[this._controls.up]) {
       this.accelerate(new $P.Coord(0, -this._acceleration));
     }
-    if ($P.keys['39']) {
+    if ($P.keys[this._controls.right]) {
       this.accelerate(new $P.Coord(this._acceleration, 0));
     }
-    if ($P.keys['40']) {
+    if ($P.keys[this._controls.down]) {
       this.accelerate(new $P.Coord(0, this._acceleration));
     }
-    if ($P.keys['37']) {
+    if ($P.keys[this._controls.left]) {
       this.accelerate(new $P.Coord(-this._acceleration, 0));
     }
   }
