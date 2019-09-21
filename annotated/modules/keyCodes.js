b3fcc9373dafbf8634b390ad00afa2a7d150ea2f
riggedtoexplode <thomas.zent@gmail.com>
2019-09-21 10:41:50 -0700

    Correct keyCodes

--- annotated/modules/keyCodes.js
+++ annotated/modules/keyCodes.js
@@ -2,12 +2,12 @@ $P.keyCodes = []; //Declare keyCodes array
 
 window.addEventListener("keydown",
     function(e) {
-        keyCodes[e.keyCode] = true; //Add a key to the array with the value of true when that key is pressed
+        $P.keyCodes[e.keyCode] = true; //Add a key to the array with the value of true when that key is pressed
     },
 false);
 
 window.addEventListener('keyup',
     function(e) {
-        keyCodes[e.keyCode] = false; //Set a key to false when that key is released
+        $P.keyCodes[e.keyCode] = false; //Set a key to false when that key is released
     },
 false);
