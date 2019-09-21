2722eb01a7fe4c698a960325f15bf5a8209701e4
riggedtoexplode <thomas.zent@gmail.com>
2019-09-21 10:42:32 -0700

    Correct keyCodes

--- modules/keyCodes.js
+++ modules/keyCodes.js
@@ -1,1 +1,1 @@
-$P.keyCodes=[],window.addEventListener("keydown",function(e){keyCodes[e.keyCode]=!0},!1),window.addEventListener("keyup",function(e){keyCodes[e.keyCode]=!1},!1);
+$P.keyCodes=[],window.addEventListener("keydown",function(e){$P.keyCodes[e.keyCode]=!0},!1),window.addEventListener("keyup",function(e){$P.keyCodes[e.keyCode]=!1},!1);
