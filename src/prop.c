82398f1696b38a6326fdb9d8b4128a5f816291b5
Thomas Zentmyer <thomas.zent@gmail.com>
2020-08-31 18:15:53 -0700

    Add EMSCRIPTEN_KEEPALIVE flags

--- src/prop.c
+++ src/prop.c
@@ -1,35 +1,36 @@
 #include <math.h>
+#include <emscripten/emscripten.h>
 
-void add(float *coord1, float *coord2) {
+void EMSCRIPTEN_KEEPALIVE add(float *coord1, float *coord2) {
     coord1[0] += coord2[0];
     coord1[1] += coord2[1];
 }
 
-void subtract(float *coord1, float *coord2) {
+void EMSCRIPTEN_KEEPALIVE subtract(float *coord1, float *coord2) {
     coord1[0] -= coord2[0];
     coord1[1] -= coord2[1];
 }
 
-void multiply(float *coord1, float *coord2) {
+void EMSCRIPTEN_KEEPALIVE multiply(float *coord1, float *coord2) {
     coord1[0] *= coord2[0];
     coord1[1] *= coord2[1];
 }
 
-void factor(float *coord, float factor) {
+void EMSCRIPTEN_KEEPALIVE factor(float *coord, float factor) {
     coord[0] *= factor;
     coord[1] *= factor;
 }
 
-void divide(float *coord1, float *coord2) {
+void EMSCRIPTEN_KEEPALIVE divide(float *coord1, float *coord2) {
     coord1[0] /= coord2[0];
     coord1[1] /= coord2[1];
 }
 
-void divisor(float *coord, float divisor) {
+void EMSCRIPTEN_KEEPALIVE divisor(float *coord, float divisor) {
     coord[0] /= divisor;
     coord[1] /= divisor;
 }
 
-float dist(float *coord1, float *coord2) {
+float EMSCRIPTEN_KEEPALIVE dist(float *coord1, float *coord2) {
     return sqrt(pow(coord2[0] - coord1[0], 2.0) + pow(coord2[1] - coord1[1], 2.0));
 }
